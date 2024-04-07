9f6a4f80a374f6afec27edc7f1b592ecd92457fa
Adrian Sutansaty <iamyourdre@gmail.com>
2024-04-07 20:00:12 +0700

    update: Hero (sm screen)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,17 +7,17 @@ const Hero = () => {
     <div className='px-[2rem] md:px-[4rem] lg:px-[8rem] font-abril my-12 '>
       <div className="flex flex-wrap">
         <div className="w-full md:w-8/12 lg:w-7/12 lg:pr-16 text-center md:text-left">
-          <p className='text-7xl md:text-8xl font-abril '>Welcome <span className='text-2xl md:text-4xl'>to</span></p>
+          <p className='text-6xl md:text-8xl font-abril '>Welcome <span className='text-2xl md:text-4xl'>to</span></p>
           <div className="flex justify-center md:justify-normal">
-            <img src={nj2} alt="nj2" className='h-[3.2rem] md:h-[4.6rem] flex-none relative top-3 md:top-2.5'/>
-            <p className='text-7xl md:text-8xl flex-initial ms-3  md:ms-4'>Official</p>
+            <img src={nj2} alt="nj2" className='h-[3rem] md:h-[4.6rem] flex-none relative top-2 md:top-2.5'/>
+            <p className='text-6xl md:text-8xl flex-initial ms-3  md:ms-4'>Official</p>
           </div>
           <div>
             <div className='bg-blue-900 text-blue-600 w-auto inline-block px-12 mt-4 rounded-full -rotate-[1.5deg] absolute translate-y-1'>
-              <p className='text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
+              <p className='text-6xl md:text-8xl inline relative bottom-1'>Fansite.</p>
             </div>
             <div className='bg-neutral-200 text-blue-600 w-auto inline-block px-12 mt-4 rounded-full -rotate-[2deg]'>
-              <p className='text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
+              <p className='text-6xl md:text-8xl inline relative bottom-1'>Fansite.</p>
             </div>
           </div>
           <p className='font-playfair py-9 md:w-10/12 lg:w-full'>Stay in the middle.. Like you a little.. Don't want no riddle.. 말해줘 say it back, oh, say it ditto.. 아침은 너무 멀어 so say it ditto.. I don't want to walk in this 미로.. 다 아는 건 아니어도.. 바라던 대로 말해줘..</p>
