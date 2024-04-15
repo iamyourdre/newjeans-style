d127a4e6084c6c7cf550ea462908fd407d71f28a
Adrian Sutansaty <iamyourdre@gmail.com>
2024-04-15 19:06:28 +0700

    update: Hero (xs screen)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,20 +7,20 @@ const Hero = () => {
     <div className='px-[2rem] md:px-[4rem] lg:px-[8rem] font-playfair font-bold my-12 '>
       <div className="flex flex-wrap">
         <div className="w-full md:w-8/12 lg:w-7/12 lg:pr-16 text-center md:text-left">
-          <p className='text-7xl md:text-8xl '>Welcome <span className='text-2xl md:text-4xl'>to</span></p>
+          <p className='text-6xl sm:text-7xl md:text-8xl '>Welcome <span className='text-2xl md:text-4xl'>to</span></p>
           <div className="flex justify-center md:justify-normal">
             <img src={nj2} alt="nj2" className='h-[3rem] md:h-[4.6rem] flex-none relative top-2 md:top-3.5'/>
-            <p className='text-7xl md:text-8xl flex-initial ms-3  md:ms-4'>Official</p>
+            <p className='text-6xl sm:text-7xl md:text-8xl flex-initial ms-3  md:ms-4'>Official</p>
           </div>
           <div className='hover:rotate-[1deg] hover:translate-y-1 ease-in duration-200'>
             <div className='bg-blue-900 text-blue-600 w-auto inline-block px-12 mt-4 rounded-full -rotate-[1deg] absolute translate-y-1'>
-              <p className='text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
+              <p className='text-6xl sm:text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
             </div>
             <div className='bg-neutral-200 text-blue-600 w-auto inline-block px-12 mt-4 rounded-full -rotate-[2deg] hover:-rotate-[1deg] hover:translate-y-1 ease-in duration-200'>
-              <p className='text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
+              <p className='text-6xl sm:text-7xl md:text-8xl inline relative bottom-1'>Fansite.</p>
             </div>
           </div>
-          <p className='py-9 md:w-10/12 lg:w-full font-thin'>Stay in the middle.. Like you a little.. Don't want no riddle.. 말해줘 say it back, oh, say it ditto.. 아침은 너무 멀어 so say it ditto.. I don't want to walk in this 미로.. 다 아는 건 아니어도.. 바라던 대로 말해줘..</p>
+          <p className='py-9 md:w-10/12 lg:w-full text-sm sm:text-base font-thin'>Stay in the middle.. Like you a little.. Don't want no riddle.. 말해줘 say it back, oh, say it ditto.. 아침은 너무 멀어 so say it ditto.. I don't want to walk in this 미로.. 다 아는 건 아니어도.. 바라던 대로 말해줘..</p>
         </div>
         <div className="w-full md:w-4/12 lg:w-5/12">
           <div className="grid grid-cols-3 gap-1 w-full">
